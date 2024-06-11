a41bbe5d6eab69ff67c8c96190ed5fc744a6d29f
Koza Kurumlu <kozakurumlu@gmail.com>
2024-06-11 09:55:57 +0100

    Initial Commit

---
+++ Breweries/BreweriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweriesApp.swift
+//  Breweries
+//
+//  Created by Kurumlu, Koza (RCH) on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BreweriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
